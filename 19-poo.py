a243255c0121575c5c1488eef42dea17384bb4ba
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2018-03-29 07:10:52 -0300

    Adicionado material de POO

--- 19-poo.py
+++ 19-poo.py
@@ -1,127 +1,126 @@
-# #Classe é o projeto de um objeto
-# #Objeto é a execução do código de uma classe
-# #Instância é sinônimo de objeto
-# #Propriedades são variáveis que armazenam atributos
-# #Métodos são as funções que o objeto pode realizar
+#Classe é o projeto de um objeto
+#Objeto é a execução do código de uma classe
+#Instância é sinônimo de objeto
+#Propriedades são variáveis que armazenam atributos
+#Métodos são as funções que o objeto pode realizar
 
-# def Pessoa():
-# 	def __init__(self): #Método construtor
-# 		pass
+def Pessoa():
+	def __init__(self): #Método construtor
+		pass
 
-# p1 = Pessoa()
-# print(id(p1))
-# p2 = Pessoa()
-# print(id(p2))
+p1 = Pessoa()
+print(id(p1))
+p2 = Pessoa()
+print(id(p2))
 
-# print("\n")
+print("\n")
 
-# class Retangulo():
-# 	def __init__(self):
-# 		self.altura  = 0
-# 		self.largura = 0
+class Retangulo():
+	def __init__(self):
+		self.altura  = 0
+		self.largura = 0
 
-# 	def area(self):
-# 		return self.altura * self.largura
+	def area(self):
+		return self.altura * self.largura
 
-# r1 = Retangulo()
+r1 = Retangulo()
 
-# #Acessando e modificando atributos:
-# r1.altura  = 10
-# r1.largura = 15 
+#Acessando e modificando atributos:
+r1.altura  = 10
+r1.largura = 15 
 
-# #Chamando métodos:
-# print( r1.area() )
-
-# ###############################################################################
-# #Método Construtor Personalizado
-
-# class Circulo():
-# 	def __init__(self, raio): #Sempre que um objeto é criado tem que passar r (dunder init dunder)
-# 		self.r = raio
-
-# 	def area(self):
-# 		return 3.14 * self.r *self.r 
-
-# circulo = Circulo(3)
-# print(circulo.area())
-
-# ###############################################################################
-# #Setters e Getters
-# class Quadrado():
-# 	def __init__(self, lado):
-# 		self.lado = 0 
-# 		self.set_lado(lado)
-
-# 	def set_lado(self, num):
-# 		if(not(isinstance(num, int) and (num > 0))):
-# 			raise ValueError("O valor de lado deve ser inteiro!") #O programa encerra
-# 		self.lado = num
-
-# 	def get_area(self):
-# 		return self.lado * self.lado  
-
-# #Visibilidade de membros (propriedades e métodos)
-# publico   = 0
-# _restrito = 0 #Esse atributo não pode ser acessado diretamente
-
-# class Quadrado():
-# 	def __init__(self, lado):
-# 		self._lado = 0 
-# 		self.set_lado(lado)
-
-# 	def set_lado(self, num):
-# 		if(not(isinstance(num, int) and (num > 0))):
-# 			raise ValueError("O valor de lado deve ser inteiro!") #O programa encerra
-# 		self._lado = num
-
-# 	def get_area(self):
-# 		return self._lado * self._lado  
-
-# #Para evitar a colisão de nomes entre a superclasse e a subclasse o membro
-# #deve ser precedido por dois underlines (dunders)
-# __colisao = 0
-
-# ###############################################################################
-# #Property
-# class A():
-# 	def __init__(self):
-# 		self._var = 0
-
-# 	def _get_var(self):
-# 		return self._var
-
-# 	def _set_var(self, num):
-# 		self._var = num
-
-# 	var = property(fget = _get_var, fset = _set_var)
-
-# a = A()
-# a.var = 10
-# print(a.var)
-# print("\n")
-# ###############################################################################
-# #Decorators
-# #O decorador de método adiciona funcionalidades ao método sem alterar sua implementação
-
-# class B():
-# 	def __init__(self):
-# 		self._var = 0
-
-# 	@property
-# 	def var(self): #Ao definir property usando decorator o nome da função deve ser igual ao da propriedade
-# 		return self._var
-
-# 	@var.setter
-# 	def var(self, num):
-# 		self._var = num
-
-# b = B()
-# b.var = 15
-# print(b.var)
+#Chamando métodos:
+print( r1.area() )
 
 ###############################################################################
-#Classe final
+#Método Construtor Personalizado
 
+class Circulo():
+	def __init__(self, raio): #Sempre que um objeto é criado tem que passar r (dunder init dunder)
+		self.r = raio
+
+	def area(self):
+		return 3.14 * self.r *self.r 
+
+circulo = Circulo(3)
+print(circulo.area())
+
+###############################################################################
+#Setters e Getters
+class Quadrado():
+	def __init__(self, lado):
+		self.lado = 0 
+		self.set_lado(lado)
+
+	def set_lado(self, num):
+		if(not(isinstance(num, int) and (num > 0))):
+			raise ValueError("O valor de lado deve ser inteiro!") #O programa encerra
+		self.lado = num
+
+	def get_area(self):
+		return self.lado * self.lado  
+
+#Visibilidade de membros (propriedades e métodos)
+publico   = 0
+_restrito = 0 #Esse atributo não pode ser acessado diretamente
+
+class Quadrado():
+	def __init__(self, lado):
+		self._lado = 0 
+		self.set_lado(lado)
+
+	def set_lado(self, num):
+		if(not(isinstance(num, int) and (num > 0))):
+			raise ValueError("O valor de lado deve ser inteiro!") #O programa encerra
+		self._lado = num
+
+	def get_area(self):
+		return self._lado * self._lado  
+
+#Para evitar a colisão de nomes entre a superclasse e a subclasse o membro
+#deve ser precedido por dois underlines (dunders)
+__colisao = 0
+
+###############################################################################
+#Property
+class A():
+	def __init__(self):
+		self._var = 0
+
+	def _get_var(self):
+		return self._var
+
+	def _set_var(self, num):
+		self._var = num
+
+	var = property(fget = _get_var, fset = _set_var)
+
+a = A()
+a.var = 10
+print(a.var)
+print("\n")
+###############################################################################
+#Decorators
+#O decorador de método adiciona funcionalidades ao método sem alterar sua implementação
+
+class B():
+	def __init__(self):
+		self._var = 0
+
+	@property
+	def var(self): #Ao definir property usando decorator o nome da função deve ser o mesmo da propriedade
+		return self._var
+
+	@var.setter
+	def var(self, num):
+		self._var = num
+
+b = B()
+b.var = 15
+print(b.var)
+
+###############################################################################
+#Exemplo:
 class Rectangle():
 	def __init__(self, largura, altura):
 		self._largura = largura
@@ -153,7 +152,7 @@ class Rectangle():
 
 my_rectangle = Rectangle(10, 20)
 my_rectangle.largura = 5
-my_rectangle.altura = 7
+my_rectangle.altura  = 7
 
 print(my_rectangle.largura)
 print(my_rectangle.altura) 
@@ -161,8 +160,8 @@ print(my_rectangle.area)
 print("\n")
 
 ###############################################################################
-#Objetos de classe
-#Os objetos de classe são compartilhados entre os objetos
+#Membros de classe
+#Os membros de classe são compartilhados entre os objetos
 
 class MinhaClasse():
 	membro_classe = 50
@@ -189,5 +188,65 @@ print("-------------------------------------------------")
 obj1.func()
 obj2.func()
 
-#Acessando e modificando objeto de classe:
+#Acessando e modificando membro de classe:
 MinhaClasse.membro_classe = 1000
+print("\n")
+
+###############################################################################
+#Métodos de Classe
+class Bichos():
+	qtd_bichos = 0
+
+	def __init__(self):  #Método construtor
+		self.add_bicho()
+
+	def __del__(self):	 #Método destrutor
+		self.del_bicho()
+		if self.qtd_bichos == 0:
+			print("Todos os bichos foram mortos!")
+
+	@classmethod
+	def add_bicho(cls):	#O parâmetro cls indica que é um parâmetro de classe
+		cls.qtd_bichos += 1
+
+	@classmethod
+	def del_bicho(cls):
+		cls.qtd_bichos -= 1
+
+b1 = Bichos()
+b2 = Bichos()
+b3 = Bichos()
+
+print(Bichos.qtd_bichos)
+print("\n")
+
+###############################################################################
+#Métodos Estáticos
+#Podem ser invocados por meio de um objeto ou da instância da classe
+
+class MetodosEstaticos():
+	
+	@staticmethod
+	def func1():
+		print("Método estático 1")
+
+	@staticmethod
+	def func2(a, b, c):
+		info = """
+		Nome da função: {nome} 
+		Quantidade de Args: {quantidade}
+		Args: {args}
+		"""
+		info = info.format(
+			nome = MetodosEstaticos.func2.__name__,
+			quantidade = MetodosEstaticos.func2.__code__.co_argcount,
+			args = MetodosEstaticos.func2.__code__.co_varnames
+		)
+		print(info)
+
+obj = MetodosEstaticos()
+obj.func1()
+MetodosEstaticos.func1()
+
+obj.func2(1, 2, 3)
+MetodosEstaticos.func2(1, 2, 3)
